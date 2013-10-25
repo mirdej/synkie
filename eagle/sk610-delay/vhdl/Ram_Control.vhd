e40f07971bf48ef6f88e7d0652ff1ce821b60faa
mirdejavel <mirdejavel@7335fc80-2deb-aed5-b100-314eeb5096d6>
2013-10-25 09:34:06 +0000

    cleaned up code, signal names

--- eagle/sk610-delay/vhdl/Ram_Control.vhd
+++ eagle/sk610-delay/vhdl/Ram_Control.vhd
@@ -26,24 +26,22 @@ use ieee.numeric_std.all;
 
 entity Ram_Controller is
 	port(
-		Clk    		: in  std_logic;
-		ResetN 		: in  std_logic;
+		Clk    			: in  std_logic;
+		ResetN 			: in  std_logic;
 		
 		Overflow		: out std_logic;
-		Oszi_Trig		: out std_logic;
---		Loopthru	: in  std_logic;
 				
-		Reset_Counter	: in std_logic;
+		Write_EnableN	: in std_logic;
 		Write_Data		: in std_logic_vector (7 downto 0);
 		Read_Data		: out std_logic_vector (7 downto 0);
 		
-		Ram_Address : out std_logic_vector(13 downto 0);  -- 12 bits Address / 2 bits BANK
-		Ram_RAS		: out std_logic;
-		Ram_CAS 	: out std_logic;
-		Ram_WE		: out std_logic;
-		Ram_Data	: inout std_logic_vector(7 downto 0);
-		Ram_Clk		: out std_logic;
-		Ram_DQM		: out std_logic
+		Ram_Address 	: out std_logic_vector(13 downto 0);  -- 12 bits Address / 2 bits BANK
+		Ram_RAS			: out std_logic;
+		Ram_CAS 		: out std_logic;
+		Ram_WE			: out std_logic;
+		Ram_Data		: inout std_logic_vector(7 downto 0);
+		Ram_Clk			: out std_logic;
+		Ram_DQM			: out std_logic
 	);
 end entity;
 
@@ -51,14 +49,6 @@ end entity;
 --																			ARCHITECTURE
 --------------------------------------------------------------------------------------------
 architecture Ram_Controller_arch of Ram_Controller is
-
-
--- burst size	   
-	-- "000" burst size of 1
-	-- "001" b.s. of 2
-	-- "010" b.s. of 4
-	-- "011" b.s. of 8
-	constant burst_size : std_logic_vector(2 downto 0) := "010";
 	
 	constant CLOCK_PERIOD : positive := 13; -- in ns
 	-- timing constants in ns:
@@ -68,9 +58,6 @@ architecture Ram_Controller_arch of Ram_Controller is
 	constant tREF : positive := 15000; -- for 1 row (for 4096 you need to divide number by 4096)        
 	constant tRFC : positive := 65; 
 	constant tWR  : positive := CLOCK_PERIOD + 7; 
-	-- sdram initialization time
-	-- fo eg.: if 100 us sdram initialization is needed, tSTARTUP_NOP should be 100000 [ns]
-	constant tSTARTUP_NOP : positive := 100000;
 	
 	-- timing constants in cycles
 	-- actual cycles will be one cycle longer (every) because of state transition time (1 cycle time)
@@ -81,7 +68,6 @@ architecture Ram_Controller_arch of Ram_Controller is
 	constant tREF_CYCLES : natural := tREF / CLOCK_PERIOD;	 --	tREF_time = tREF_CYCLES + 1
 	constant tRFC_CYCLES : NATURAL := tRFC / CLOCK_PERIOD;	 -- tRFC_time = tRFC_CYCLES + 1
 	constant tWR_CYCLES  : natural := tWR / CLOCK_PERIOD; 	 --	tWR_time = tWR_CYCLES + 1
-	--constant tSTARTUP_NOP_CYCLES : positive := 10;
 	constant tSTARTUP_NOP_CYCLES : positive := 8000;
 
 	constant CAS_LATENCY : positive := 3; 
@@ -134,7 +120,7 @@ begin
 		
 		
 	-- ----------------------------------------------------------------- FINITE STATE MACHINE
-	process(slow_clk, ResetN,Reset_Counter)
+	process(slow_clk, ResetN,Write_EnableN)
 	begin
 		if (ResetN	= '0') then			
 			ram_state <= init;
@@ -160,7 +146,6 @@ begin
 				when nop =>
 					Ram_RAS <= '1'; 	Ram_CAS <= '1';		Ram_WE <= '1';	
 					Ram_DQM <= '1';
-					Oszi_Trig <= '0';
 					
 					if (ram_nops = 0) then
 						ram_state <= ram_next_state;
@@ -226,7 +211,6 @@ begin
 				when set_mode_register =>
 					Ram_RAS <= '0';		Ram_CAS <= '0';		Ram_WE  <= '0'; 
 					address_temp <= "00000011000000";
-				--	address_temp (7 downto 6) <= "11";				-- set bits 5 and 4 of Mode register high for CAS latency of 3 
 					ram_nops <= tMRD_CYCLES;
 					ram_state <= nop;
 					ram_next_state <= precharge;
@@ -241,7 +225,6 @@ begin
 
 					-- prepare Row for next read
 					address_temp (13 downto 0) 		<= byte_counter(23 downto 12) & byte_counter(1 downto 0);		-- Row Address
-					--address_temp (1 downto 0) 		<= byte_counter(1 downto 0);			-- Bank
 					ram_nops <= tRCD_CYCLES;
 					ram_state <= nop;
 					ram_next_state <= ram_read;
@@ -270,10 +253,6 @@ begin
 				---------------------------------			
 				when ram_get_data =>
 					Ram_RAS <= '1';		Ram_CAS <= '1';		Ram_WE <= '1';			-- nop
-					--ram_nops <= 2;
-					--ram_state <= nop;
-					--ram_next_state <= toggle_OE;
-					Oszi_Trig <= '1';
 					load_enable <= '1';
 					ram_state <= toggle_OE;
 				
@@ -287,7 +266,7 @@ begin
 				-- Write
 				---------------------------------			
 				when ram_write =>
-					if (Reset_Counter = '0') then
+					if (Write_EnableN = '0') then
 						Ram_RAS <= '1';		Ram_CAS <= '0';		Ram_WE <= '0';
 					else
 						Ram_RAS <= '1';		Ram_CAS <= '1';		Ram_WE <= '1';			-- nop
@@ -296,7 +275,6 @@ begin
 					ram_nops <= 1;
 					ram_state <= nop;
 					ram_next_state <= precharge;
-					--Ram_Data <= ad_buf;
 	
 				when others => null;
 			end case;
@@ -319,7 +297,7 @@ begin
 	begin
 		if ((slow_clk'event) and (slow_clk = '0')) then
 			if (load_enable = '1') then
-				if (Reset_Counter = '0') then
+				if (Write_EnableN = '0') then
 					Read_Data <= Write_Data;			-- show live out when recording
 				else
 					Read_Data <= read_buf;
@@ -328,22 +306,10 @@ begin
 		end if;
 	end process;
 	
-	process(OEn, ResetN)
-	begin
-	if (ResetN = '0') then
-		reset_buf <= "00";
-	elsif ((OEn'event) and (OEn = '1')) then
---			reset_buf(1) <= reset_buf(0);
---			reset_buf(0) <= Reset_Counter;
-		end if;
-	end process;
-	
 	Ram_clk <= not slow_clk;
 	Ram_Address <= address_temp;
 	write_buf <= Write_Data;
 
 	Overflow <= blink;
-	
-	--top_count <= "0000" & top & "000000000000";
-	
+		
 end architecture Ram_Controller_arch;
