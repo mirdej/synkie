e6cd2e6ad6c1b180c8fb3e98e0d3d70b693516df
mirdejavel <mirdejavel@7335fc80-2deb-aed5-b100-314eeb5096d6>
2013-11-09 15:32:39 +0000

    synchronized record button to odd/even signal to prevent visible glitches

--- eagle/sk505-lut/firmware/main.c
+++ eagle/sk505-lut/firmware/main.c
@@ -20,17 +20,12 @@
 // Globals
 // ------------------------------------------------------------------------------
 
-uint8_t 	table [256];
-volatile uint16_t	ref_in;
-volatile uint8_t 	x_in, y_in;
-uint8_t 	ad_idx;
-volatile uint8_t 	is_recording;
-uint8_t		test;
+#define	IN_MAX 930  		// voltage reference is 1.1V - cv-max is 1V
 
-
-volatile unsigned long milliseconds; 
-ISR(TIMER1_COMPA_vect)  { milliseconds++;} 
-unsigned long millis() { return milliseconds; } 
+uint8_t 			table [256];
+uint8_t 			x_in, y_in;
+uint8_t 			ad_idx;
+volatile uint8_t 			is_recording;
 
 // ==============================================================================
 // Functions
@@ -42,8 +37,8 @@ void ad_start_conversion() {
 }
 
 void ad_set_channel(uint8_t chan) {
-	ADMUX 	= (1 << REFS1) | (chan & 0x03); 			// Internal 1.1V Voltage Reference.
-														// Cannot use external reference (must be min 2 Volts)
+	ADMUX 	= (1 << REFS1) | (chan & 0x03); 			// Internal 1.1V Voltage IN_MAX.
+														// Cannot use external IN_MAX (must be min 2 Volts)
 }
 
 uint16_t ad_Read10bit (void) {
@@ -53,7 +48,7 @@ uint16_t ad_Read10bit (void) {
 uint8_t to_scale(uint16_t in) {	
 	uint32_t t;
 	t = (uint32_t)in * 255;
-	t /= ref_in;
+	t /= IN_MAX;
 	if (t > 255) t = 255;
 	return (uint8_t)t ;
 }
@@ -67,14 +62,11 @@ void check_ad(void){
 		uint16_t temp;
 		temp = ad_Read10bit();
 		
-		if (ad_idx == 1) {
-			ref_in = temp;
-			ad_idx = 2;
-		} else if (ad_idx == 2) {	// 
+		if (ad_idx == 2) {	// 
 				ad_idx = 3;
 				y_in = to_scale(temp);
 		} else {
-				ad_idx = 1;
+				ad_idx = 2;
 				x_in = to_scale(temp);
 		}
 		
@@ -82,7 +74,7 @@ void check_ad(void){
 		ad_start_conversion();
 }
 
-void check_btn(void){
+ISR(INT0_vect){
 	is_recording = ~PINB & (1 << 0);
 }
 
@@ -104,23 +96,21 @@ void pwm_off(void) {
 void init (void) {
 	// setup Ports
 	// 	PB5 	PB4		PB3		PB2		PB1		PB0
-	//	RESET	Y-IN	X-IN	REF		Y-OUT	BTN
+	//	RESET	Y-IN	X-IN	Sync	Y-OUT	BTN
 	
 	DDRB = 	(1 << 1);	// output for PWM
 	PORTB = (1 << 0); 	// pullup on Button
 	
 	// setup analog converter. 
-	ADCSRA 	= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1); 		// enable adc, prescaler 64
+	ADCSRA 	= (1 << ADEN) | (1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0); 		// enable adc, prescaler 16
 
-	ad_idx = 1;
+	ad_idx = 2;
 	ad_set_channel(ad_idx);
 	ad_start_conversion();
 
-	
-	// setup timer 1 for debouncing stuff
-    TCCR1 = (1<<CTC1)|(7<<CS10);   // CTC  mode, div64 
-    OCR1C = 0.001 * F_CPU/64 - 1;  // 1ms, F_CPU @16MHz, div64 
-    TIMSK |= (1<<OCIE1A); 
+	// INT 0 interrupt on V-Sync Input
+	MCUCR = (1 << ISC01) | (1 << ISC00); // rising edge of odd/even generates interrupt
+	GIMSK = (1 << INT0);				// enable interrupt
 	sei();
 }
 
@@ -129,36 +119,31 @@ void init (void) {
 // ------------------------------------------------------------------------------
 
 int main (void) {
-	init();
 	
 	unsigned int last_millis;
 	unsigned int now;
 	unsigned char y_out;
+		
+	init();
+	y_in = 220;
 	
 	while(1) {
+		check_ad();
 	
-		check_ad();	
-		now = millis();
-		
-		if (now - last_millis > 4) {
-			last_millis = now;
-			check_btn();
-			
-			if (is_recording) { 
-				table[x_in] = y_in;
-			}
-			y_out = table[x_in];
-			
-			if (y_out == 0) {
-				pwm_off();
-				PORTB &= ~(1 << 1);			// turn off pin
-			} else if (y_out == 255) {
-				pwm_off();
-				PORTB |= (1 << 1);			// turn on pin
-			} else {
-				OCR0B = y_out;
-				pwm_on();
-			}
+		if (is_recording) { 
+			table[x_in] = y_in;
+		}
+		y_out = table[x_in];
+	 
+		if (y_out == 0) {
+			pwm_off();
+			PORTB &= ~(1 << 1);			// turn off pin
+		} else if (y_out == 255) {
+			pwm_off();
+			PORTB |= (1 << 1);			// turn on pin
+		} else {
+			OCR0B = y_out;
+			pwm_on();
 		}
 	}
 	return 0;
